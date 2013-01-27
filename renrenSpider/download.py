8e1e190ff6ccddab7c3276be6a3019d2cdb8b233
JackonYang <jiekunyang@gmail.com>
2013-01-27 11:51:51 +0800

    homepage, profile_detail  done.

--- renrenSpider/download.py
+++ renrenSpider/download.py
@@ -6,25 +6,25 @@ import http.cookiejar
 import re
 import time
 
-import mytools #used to get passwd from personal mysql
-
 timeout=3
 urls={
-	'status':'http://status.renren.com/status?curpage={}&id={}&__view=async-html',
-	'friendList':"http://friend.renren.com/GetFriendList.do?curpage={}&id={}",
-	'profile_info':"http://www.renren.com/{}/profile?v=info_ajax",
 	'login':"http://www.renren.com/PLogin.do",
+	'friendList':"http://friend.renren.com/GetFriendList.do?curpage={}&id={}",
+	'profile_detail':"http://www.renren.com/{}/profile?v=info_ajax",
+	'status':'http://status.renren.com/status?curpage={}&id={}&__view=async-html',
 	'homepage':"http://www.renren.com/{}/profile"}
+itemReg={
+	'status':r'id="status-.+?ilike_icon',
+	'friendList':re.compile(r'<dd><a\s+href=\"http://www.renren.com/profile.do\?id=\d+\">.+?<\/a>'),
+	'profile_detail':re.compile(r'<dl\sclass="info">.*?</dl>',re.DOTALL),
+	'homepage_tl':re.compile(r'<ul class="information-ul".*?</ul>',re.DOTALL),
+	'homepage_basic':re.compile(r'<(?:u|d)l class="(?:user-info )*?clearfix">.*?</(?:u|d)l>',re.DOTALL)}
 
 class download:
-	itemReg={
-		'status':r'id="status-.+?ilike_icon',
-		'friendList':re.compile(r'<dd><a\s+href=\"http://www.renren.com/profile.do\?id=\d+\">.+?<\/a>'),
-		'profile_info':re.compile(r'<dl\sclass="info">.*?</dl>',re.DOTALL),
-		'homepage':re.compile(r'<ul class="information-ul".*?</ul>',re.DOTALL)}
 
 	def __init__(self,user='yyttrr3242342@163.com',passwd=None):
 		if passwd is None:
+			import mytools #used to get passwd from personal mysql
 			passwd=mytools.getPasswd('renren',user)
 		renrenId,reason=self.login(user,passwd)
 		if renrenId is None:
@@ -32,14 +32,16 @@ class download:
 
 	def friendList(self,renrenId='285060168',targetPage=None, uppage=100):
 		"""friendList('234234') -->
-		return friendList,timecost:total/download/perpage if success
-		return set(),privacy if can't access because of privacy policy
-		return set(),expt={},actual={} if some pages parse error"""
+		return (friendList,info) if success
+		return (set(),'privacy') if can't access because of privacy policy
+		return (set(),info) if some pages parse error
+		return (set(),'timeout') if timeout"""
 		pageStyle='friendList'
 		if targetPage==None:
 			return self.iterPage(pageStyle,renrenId,uppage)
 		else:
 			return self.onePage(urls[pageStyle].format(curpage,renrenId))
+			#parse
 
 	def status(self,renrenId=None,targetPage=None, uppage=100):
 		pageStyle='status'
@@ -48,68 +50,76 @@ class download:
 		else:
 			return self.onePage(urls[pageStyle].format(curpage,renrenId))
 
-	def profile(self,renrenId):
-		pageStyle='profile_info'
-		html_content,time=self.onePage(urls[pageStyle].format(renrenId))
+	def profile_detail(self,renrenId):
+		"""profile_detail('234234') -->
+		return (items,'success') if success
+		return (set(),'privacy') if can't access because of privacy policy
+		return (set(),'timeout') if timeout"""
+		pageStyle='profile_detail'
+		html_content,info=self.onePage(urls[pageStyle].format(renrenId))
 		if html_content is None:
-			print(time)
-			return set(),'timeout'
-		elif html_content[0:30].find('<div class="col-left">')==-1:
-			return set(),'privacy'
-		else:
-			#parse
-			items_curpage=self.itemReg[pageStyle].findall(html_content)
+			return set(),info
+		elif html_content[0:30].find('<div class="col-left">') > -1:
+			items_curpage=itemReg[pageStyle].findall(html_content)
 			return items_curpage,'success'
+		else:
+			return set(),'privacy'
 
 	def homepage(self,renrenId):
 		pageStyle='homepage'
-		html_content,time=self.onePage(urls[pageStyle].format(renrenId))
-		#print('{},{}'.format(renrenId,html_content[0:50]))
-		#TODO:deal with parse
+		html_content,info=self.onePage(urls[pageStyle].format(renrenId))
+		if html_content is None:
+			return set(),info
+		elif html_content[0:30].find('<!doctype html><html>') > -1:#tl
+			items_curpage=itemReg[pageStyle+'_tl'].findall(html_content)
+			return items_curpage,'tl'
+		else:
+			items_curpage=itemReg[pageStyle+'_basic'].findall(html_content)
+			return items_curpage,'basic'
 
 	def onePage(self,url):
-		startTime=time.time()
 		try:
 			rsp=self.opener.open(url)
-			htmlStr=rsp.read().decode('UTF-8','ignore')
+			#rspurl=rsp.geturl()
+			html_content=rsp.read().decode('UTF-8','ignore')
 		except urllib.error.URLError as e:
 			return None,'timeout'
 		#TODO: deal with resend
 		else:
-			stopTime=time.time()
-			return htmlStr,stopTime-startTime
+			return html_content,'success'
 
 	def iterPage(self,pageStyle=None,renrenId=None,uppage=100):
-		looptime=0
-		start=time.time()
 		itemsAll=set()
-		for curpage in range(uppage+1):
-			html_content,timecost=self.onePage(urls[pageStyle].format(curpage,renrenId))
+		request_time=[]
+		for curpage in range(uppage):
+			request_start=time.time()
+			html_content,info=self.onePage(urls[pageStyle].format(curpage,renrenId))
+			request_time.append(time.time()-request_start)
 			if html_content is None:
-				return set(),'{} ,page={},renrenId={},{}.'.format(pageStyle,curpage,renrenId,timecost)
+				return set(),info
 			else:
-				items_curpage=self.itemReg[pageStyle].findall(html_content)
-			if len(items_curpage) < 1:#all pages request
-				stop=time.time()
-				if html_content.find('f-privacy-tip')>0:
-					return set(),'privacy'
-				break
-			else:
-				looptime += timecost
-				n=len(items_curpage)
-				itemsAll=itemsAll | set(items_curpage)
+				items_curpage=itemReg[pageStyle].findall(html_content)
+				if len(items_curpage) < 1:#all pages request
+					if html_content.find('f-privacy-tip')>0:
+						return set(),'privacy'
+					break
+				else:
+					itemsAll=itemsAll | set(items_curpage)
+					last_num=len(items_curpage)
+
 		#check data
-		if len(itemsAll) == 20*curpage-20+n:#success
-			return itemsAll,'timecost:{}/{}/{}(total/download/perpage)'.format(stop-start,looptime,looptime/curpage)
+		if len(itemsAll) == 20*curpage-20+last_num:#success
+			#return itemsAll,'timecost:{}/{}/{}(max/min/ave)'.format(0,0,0)
+			return itemsAll,'timecost:{}'.format(request_time)
 		else:
-			return {},'parse error.expt={},actual={}'.format(curpage*20-20+n,len(itemsAll))
+			return set(),'parse error.expt={},actual={}'.format(curpage*20-20+n,len(itemsAll))
 
 	def login(self,user,passwd):
-		"""return renrenId and set build default opener if login successfully.
-		return None if failed."""
+		"""return (renrenId,'success') if login successfully.
+		return (None,reason) if failed."""
 		data=urlencode({"email":user,"password":passwd}).encode(encoding='UTF8');
-		cj=http.cookiejar.CookieJar();
-		self.opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj));
+		hdlr_cookie=urllib.request.HTTPCookieProcessor(http.cookiejar.CookieJar())
+		self.opener=urllib.request.build_opener(hdlr_cookie)
 		self.opener.addheaders=[('User-agent','Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0')];
 		try:
 			rsp=self.opener.open(urls['login'],data,timeout=timeout)
@@ -120,7 +130,6 @@ class download:
 			#check whether login is successful. 
 			m=re.compile(r'http://www.renren.com/(\d+)').match(url)
 			if m is None:
-				return None,url
+				return None,'login failed,rsp={}'.format(url)
 			else:
 				return m.group(1),'success'
-
--- renrenSpider/testDownload.py
+++ renrenSpider/testDownload.py
@@ -9,19 +9,19 @@ class Test_download(unittest.TestCase):
 	def tearDown(self):
 		pass
 
-	def test_profile(self):
+	def test_profile_detail(self):
 		renrenIds={'233330059','230760442','223981104','410941086','285060168'}
 							#myself,timeline ok/unavailable,old style ok/unavailable
 		for rid in renrenIds:
-			info,reason=self.dl.profile(rid)
+			info,reason=self.dl.profile_detail(rid)
 			print('{},{},{}'.format(rid,len(info),reason))
 
 	def test_friendList(self):
 		renrenIds={'233330059','410941086','267654044','285060168','240303471'}
 							#myself,3+pages/2pages/1page/unavailable
 		for rid in renrenIds:
-			fl,reason=self.dl.friendList(rid)
-			print('{},{},{}'.format(rid,len(fl),reason))
+			fl,info=self.dl.friendList(rid)
+			print('{},{},{}'.format(rid,len(fl),info))
 		#flist={'232639310':35,'242543024':152,'285060168':5}
 		#for item in flist.items():
 			#self.assertEqual(len(self.browser.friendList(item[0])),item[1])
@@ -29,11 +29,8 @@ class Test_download(unittest.TestCase):
 	def test_homepage(self):
 		renrenIds={'233330059','410941086','267654044','285060168','240303471'}
 		for rid in renrenIds:
-			#TODO:deal with parse test
-			self.dl.homepage(rid)
-			#fl,reason=self.dl.friendList(rid)
-			#print('{},{},{}'.format(rid,len(fl),reason))
-		
+			info,reason=self.dl.homepage(rid)
+			print('{},{},{}'.format(rid,len(info),reason))
 
 	def test_login(self):
 		users=['yyttrr3242342@163.com','jiekunyang@gmail.com','zhangxiaoxu_521@yahoo.com.cn','none@adaf.com']
@@ -43,9 +40,9 @@ class Test_download(unittest.TestCase):
 
 if __name__=='__main__':
 	suite=unittest.TestSuite()
-	#suite.addTest(Test_download('test_profile'))
-	suite.addTest(Test_download('test_homepage'))
 	#suite.addTest(Test_download('test_friendList'))
+	suite.addTest(Test_download('test_profile_detail'))
+	suite.addTest(Test_download('test_homepage'))
 	#suite.addTest(Test_download('test_login'))
 	runner=unittest.TextTestRunner()
 	runner.run(suite)
