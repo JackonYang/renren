55c0a08f30edb0a0ff4af2eca299c367b7df6de0
JackonYang <jiekunyang@gmail.com>
2013-02-03 22:50:03 +0800

    single thread, get friendlist, runlog is writtern

--- renrenSpider/spider.py
+++ renrenSpider/spider.py
@@ -1,21 +1,48 @@
-import download
 import time
+import logging
+
+import download
+import database
+
+
+def runlog(tt='run'):
+		logfile='run.log'
+		logger=logging.getLogger(tt)
+		hdlr=logging.FileHandler(logfile)
+		formatter=logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+		hdlr.setFormatter(formatter)
+		logger.addHandler(hdlr)
+		logger.setLevel(20)#20 info, 40 error
+		return logger
 
 user='jiekunyang@gmail.com'
 dl=download.download(user)
+save=database.database('renren_orig')
+log=runlog()
 
-fl_searched=set()#db.getSearched('relation')
+
+fl_searched=save.getSearched('friendList')
 
 def getNet2(rid='410941086'):
+	log.debug('{} start to search net2 of {}'.format(time.strftime('%H:%M:%S',time.localtime()),rid))
+	timeout_list=set()
 	fl,timecost=dl.friendList(rid)
-	#db.insertFriendList(rid,fl)
+	save.friendList(rid,fl)
 	toSearch=set(fl.keys())-fl_searched
-	print('renrenId={},toSearch/total:{}/{}'.format(rid,len(toSearch),len(fl)))
+	log.info('{} renrenId={},toSearch/total:{}/{}'.format(time.strftime('%H:%M:%S',time.localtime()),rid,len(toSearch),len(fl)))
 	for i,item in zip(range(len(toSearch)),toSearch):
 		friends,timecost=dl.friendList(item)
-		print('{}/{} {},{},{},{} friends,{}'.format(i,len(toSearch),time.strftime('%H:%M:%S',time.localtime()),item,fl[item],len(friends),timecost))
-		#db.insertFriendList(item,friends)
+		if friends is None:
+			timeout_list.add(rid)
+		else:
+			n=save.friendList(item,friends)
+			log.info('{}/{} {},{},{} friends,{} saved,{}'.format(i,len(toSearch),item,fl[item],len(friends),n,timecost))
+	log.info('{} net2 of {} done'.format(time.strftime('%H:%M:%S',time.localtime()),rid))
+	#TODO:deal with timeout list
+	#print('timeout list: {}'.format(timeout_list()))
+
 
 if __name__ == '__main__':
-	rid=input('renrenId= ')
-	getNet2(rid)
+	#rid=input('renrenId= ')
+	getNet2('233330059')
+	save.close()
