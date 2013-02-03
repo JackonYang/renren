9c37c64caa7a1b248c61dd1984ccd3d051ef9c64
JackonYang <jiekunyang@gmail.com>
2013-02-03 23:06:34 +0800

    optimise runlog of spider.py

--- renrenSpider/spider.py
+++ renrenSpider/spider.py
@@ -29,14 +29,14 @@ def getNet2(rid='410941086'):
 	fl,timecost=dl.friendList(rid)
 	save.friendList(rid,fl)
 	toSearch=set(fl.keys())-fl_searched
-	log.info('{} renrenId={},toSearch/total:{}/{}'.format(time.strftime('%H:%M:%S',time.localtime()),rid,len(toSearch),len(fl)))
+	log.debug('{} renrenId={},toSearch/total:{}/{}'.format(time.strftime('%H:%M:%S',time.localtime()),rid,len(toSearch),len(fl)))
 	for i,item in zip(range(len(toSearch)),toSearch):
 		friends,timecost=dl.friendList(item)
 		if friends is None:
 			timeout_list.add(rid)
 		else:
 			n=save.friendList(item,friends)
-			log.info('{}/{} {},{},{} friends,{} saved,{}'.format(i,len(toSearch),item,fl[item],len(friends),n,timecost))
+			log.info('{}/{} download/saved:{}/{},net2 of {},{},{},{}'.format(i,len(toSearch),len(friends),n,rid,item,fl[item],timecost))
 	log.info('{} net2 of {} done'.format(time.strftime('%H:%M:%S',time.localtime()),rid))
 	#TODO:deal with timeout list
 	#print('timeout list: {}'.format(timeout_list()))
