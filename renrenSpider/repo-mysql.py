cdc16dddd67ca3fb765eff086f19159a22c3b8ed
JackonYang <jiekunyang@gmail.com>
2013-02-10 23:24:27 +0800

    save-pageStyle done.un-tested

--- renrenSpider/repo-mysql.py
+++ renrenSpider/repo-mysql.py
@@ -1,92 +1,89 @@
 import pymysql
-pf_details={
-	#basic$
-	'生日':'birth','家乡':'hometown','性别':'gender',
-	#edu
-	'大学':'edu_college','高中':'edu_senior','中专技校':'edu_tech','初中':'edu_junior','小学':'edu_primary',
-	#work
-	'公司':'company','时间':'work_time',
-	#contact
-	'QQ':'qq','MSN':'msn','手机号':'phone','个人网站':'personal_website','我的域名':'domain1','个性域名':'domain2'
-}
-pf_mini={'location':'nowCity','address':'nowCity','work':'nowCompany','school':'nowSchool','birth':'birth','hometown':'hometown','gender':'gender'}
 
-class database:
-	sqls=dict()
-	sqls['name']='CREATE TABLE if not exists {} (renrenId1 varchar(15) NOT NULL,name varchar(20),lastmodified TIMESTAMP DEFAULT NOW() {})ENGINE=InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;'
-	sqls['friendList']='CREATE TABLE if not exists {} (renrenId1 varchar(15) NOT NULL,renrenId2 varchar(15) NOT NULL,KEY one(renrenId1),KEY two(renrenId2),lastmodified TIMESTAMP DEFAULT NOW() {} )ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;'
-	sqls['profile_detail']='CREATE TABLE if not exists {} (renrenId1 varchar(20) NOT NULL,'+' varchar(100),'.join(set(pf_details.values()))+' varchar(100),lastmodified TIMESTAMP DEFAULT NOW() {})ENGINE=InnoDB DEFAULT CHARSET = utf8;'
-	sqls['profile_mini']='CREATE TABLE if not exists {} (renrenId1 varchar(20) NOT NULL,'+' varchar(100),'.join(set(pf_mini.values()))+' varchar(100),lastmodified TIMESTAMP DEFAULT NOW() {})ENGINE=InnoDB DEFAULT CHARSET = utf8;'
-	sqls['profile_empty']='CREATE TABLE if not exists {} (renrenId1 varchar(20) NOT NULL,pfStyle varchar(20),lastmodified TIMESTAMP DEFAULT NOW() {})ENGINE=InnoDB DEFAULT CHARSET = utf8;'
-	sqls['status']='CREATE TABLE if not exists {} (statusId varchar(20) NOT NULL,renrenId1 varchar(20),timestamp varchar(20),cur_name varchar(50),cur_content varchar(500),orig_owner varchar(20),orig_name varchar(50),orig_content varchar(500),lastmodified TIMESTAMP DEFAULT NOW(),KEY cur_owner(renrenId1),KEY orig_owner(orig_owner) {})ENGINE=InnoDB DEFAULT CHARSET = utf8;'
-	key={'name':',KEY idx_temp(renrenId1)','friendList':',KEY idx_temp (renrenId1,renrenId2)','profile_detail':',KEY (renrenId1)','status':',key (statusId)','profile_mini':',KEY (renrenId1)','profile_empty':',KEY (renrenId1)'}
-	primary={'name':',PRIMARY KEY (renrenId1)','profile_detail':',PRIMARY KEY (renrenId1)','profile_mini':',PRIMARY KEY (renrenId1)','profile_empty':',PRIMARY KEY (renrenId1)','friendList':',PRIMARY KEY (renrenId1,renrenId2)','status':',PRIMARY KEY (statusId)'}
-
+class repo_mysql:
 	def __init__(self,namePre='test'):
-		self.mainTable,self.tempTable=self._init_table_name(namePre)
+		self.namePre=namePre
+		self.table=dict()
+		self.conn=self._getConn()
 		self.cur=self.conn.cursor()
-	def close(self):
+	def __del__(self):
 		self.cur.close()
 		self.conn.close()
 
-	def save_friendList(self,names,renrenId):
-		"""insert into db, and return rows affected.
-		save nothing and return 0 if empty.
+	def save_friendList(self,record,rid):
+		"""save record and return rows affected.save nothing if empty.
 		return None if input error"""
-		if names is None:
+		if record is None:
 			return None
-		elif not isinstance(names,dict):
+		elif not isinstance(record,dict):
 			return None
-		elif names == {}:
+		elif record == {}:
 			return 0
-		valFl='),({},'.format(renrenId).join(names.keys())
-		sqlFl='insert into {} (renrenId1,renrenId2) values ({},{})'.format(self.tempTable['friendList'],renrenId,valFl)
-		valNm=str(set(names.items())).strip('{}')
-		sqlNm='insert into {} (renrenId1,name) values {}'.format(self.tempTable['name'],valNm)
+		pageStyle='friendList'
+		if pageStyle not in self.table:
+			self.createTable(pageStyle)
+		val_relation='),({},'.format(rid).join(record.keys())
+		sql_relation='insert into {} (renrenId1,renrenId2) values ({},{})'.format(self.table[pageStyle],rid,val_relation)
+		val_name=str(set(record.items())).strip('{}')
+		sql_name='insert into {} (renrenId1,name) values {}'.format(self.table['name'],val_name)
 		try:
-			#print('database.friendList()')
-			#n=0
-			n=self.cur.execute(sqlFl)
-			self.cur.execute(sqlNm)
+			n=self.cur.execute(sql_relation)
+			self.cur.execute(sqr_name)
 		except Exception as e:
-			print(sqlFl)
-			print(sqlNm)
+			print(sql_relation)
+			print(sql_name)
 			return None
 		else:
 			self.conn.commit()
 			return n
-	def save_status(self,stats,renrenId=None):
-		if stats is None:
+
+	def save_status(self,record,rid=None):
+		if record is None:
 			return None
-		elif not isinstance(stats,dict):
+		elif not isinstance(record,dict):
 			return None
-		elif stats == {}:
+		elif record == {}:
 			return 0
+		pageStyle='status'
+		if pageStyle not in self.table:
+			self.createTable(pageStyle)
 		saved=0
-		for statusId,stat in stats.items():
-			valStat="statusId='{}'".format(statusId)
+		for statusId,stat in record.items():
+			val_stat="statusId='{}'".format(statusId)
 			for tag,value in stat.items():
 				if value is not None:
 					value=value.replace("'","\\'").rstrip('\\')#format , and \ 
-				valStat += ",{}='{}'".format(tag,value)
-			sqlStat='insert into {} set {}'.format(self.tempTable['status'],valStat)
+				val_stat += ",{}='{}'".format(tag,value)
+			sql_stat='insert into {} set {}'.format(self.table[pageStyle],val_stat)
 			try:
-				saved += self.cur.execute(sqlStat)
-				#print('database.status()')
+				saved += self.cur.execute(sql_stat)
 			except Exception as e:
 				print(sqlStat)
-				return 0
+				return None
 		self.conn.commit()
 		return saved
 
-	def friendList_searched(self,pageStyle='friendList'):
-		tables={self.mainTable[pageStyle],self.tempTable[pageStyle]}
-		res=set()
-		for table in tables:
-			self.cur.execute("SELECT renrenId1 FROM {} group by renrenId1".format(table))
-			for item in self.cur.fetchall():
-				res.add(item[0])
-		return res
+	def save_profile(self,record,rid):
+		"""save profile and return rows affected.return None if input error"""
+		if record is None:
+			return None
+		elif not isinstance(record,dict):
+			return None
+		elif record == {}:
+			return 0
+		pageStyle='profile'
+		val_pf="renrenId1='{}'".format(rid)
+		for tag,value in record.items():
+			val_pf += ",{}='{}'".format(pf_cols.get(tag,'extra'),value)
+		sql_pf='insert into {} set {}'.format(self.tempTable[pfStyle],val_pf)
+		try:
+			n=self.cur.execute(sql_pf)
+		except Exception as e:
+			print(sql_pf)
+			return None
+		else:
+			self.conn.commit()
+			return n
 
 	def getSearched(self,pageStyle):
 		if pageStyle=='profile':
@@ -99,44 +96,6 @@ class database:
 			for item in self.cur.fetchall():
 				res.add(item[0])
 		return res
-
-	def profile(self,renrenId,pf,pfStyle):
-		"""insert into db, and return rows affected.return None if input error"""
-		if pf is None:
-			return None
-		elif not isinstance(pf,dict):
-			return None
-		elif pf == {}:
-			return 0
-		valPf="renrenId1='{}'".format(renrenId)
-		if pfStyle == 'profile_detail':
-			for tag,value in pf.items():
-				valPf += ",{}='{}'".format(pf_details[tag],value)
-		elif pfStyle == 'profile_mini':
-			for tag,value in pf.items():
-				valPf += ",{}='{}'".format(pf_mini[tag],value)
-		else:
-			return None
-		sqlPf='insert into {} set {}'.format(self.tempTable[pfStyle],valPf)
-		try:
-			n=self.cur.execute(sqlPf)
-		except Exception as e:
-			print(sqlPf)
-			return None
-		else:
-			self.conn.commit()
-			return n
-
-	def profile_empty(self,renrenId,pfStyle):
-		sqlPf="insert into {} set renrenId1='{}',pfStyle='{}'".format(self.tempTable['profile_empty'],renrenId,pfStyle)
-		try:
-			n=self.cur.execute(sqlPf)
-		except Exception as e:
-			print(sqlPf)
-			return None
-		else:
-			self.conn.commit()
-			return n
 
 	def getFriendList(self,renrenId):
 		return self._getRenrenId(2,renrenId)
@@ -151,19 +110,34 @@ class database:
 				res.add(item[0])
 		return res
 
-	def _createTable(self):
-
-	def _init_table(self,pageStyle,namePre='test'):
-		mainTable=dict()
-		tempTable=dict()
-		for table in self.sqls.keys(): 
-			mainTable[table]='{}_main_{}'.format(namePre,table)
-			tempTable[table]='{}_temp_{}'.format(namePre,table)
-		for attr in self.sqls.keys():
-			self.cur.execute(self.sqls[attr].format(self.tempTable[attr],self.key[attr]))
-			self.cur.execute(self.sqls[attr].format(self.mainTable[attr],self.key[attr]))
-		return mainTable,tempTable
+	def _createTable(self,pageStyle):
+		self.table[pageStyle]='{}{}'.format(self.namePre,pageStyle)
+		self.cur.execute(self.sqls[pageStyle].format(self.table[pageStyle]))
 
 	def _getConn(self,usr='root',passwd='Kunth123',db='data_bang'):
-		return conn=pymysql.connect(host='127.0.0.1',port=3306,user=usr,passwd=passwd,db=db,charset='utf8')
+		return pymysql.connect(host='127.0.0.1',port=3306,user=usr,passwd=passwd,db=db,charset='utf8')
 
+pf_cols={
+	#basic$
+	'生日':'birth','家乡':'hometown','性别':'gender',
+	#edu
+	'大学':'edu_college','高中':'edu_senior','中专技校':'edu_tech','初中':'edu_junior','小学':'edu_primary',
+	#work
+	'公司':'company','时间':'work_time',
+	#contact
+	'QQ':'qq','MSN':'msn','手机号':'phone','个人网站':'personal_website','我的域名':'domain1','个性域名':'domain2'
+}
+pf_mini={'location':'nowCity','address':'nowCity','work':'nowCompany','school':'nowSchool','birth':'birth','hometown':'hometown','gender':'gender'}
+sqls=dict()
+sqls['name']='CREATE TABLE if not exists {} (renrenId1 varchar(15) NOT NULL,name varchar(20),lastmodified TIMESTAMP DEFAULT NOW() {})ENGINE=InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;'
+sqls['friendList']='CREATE TABLE if not exists {} (renrenId1 varchar(15) NOT NULL,renrenId2 varchar(15) NOT NULL,KEY one(renrenId1),KEY two(renrenId2),lastmodified TIMESTAMP DEFAULT NOW() {} )ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;'
+sqls['profile_detail']='CREATE TABLE if not exists {} (renrenId1 varchar(20) NOT NULL,'+' varchar(100),'.join(set(pf_cols.values()))+' varchar(100),lastmodified TIMESTAMP DEFAULT NOW() {})ENGINE=InnoDB DEFAULT CHARSET = utf8;'
+sqls['profile_mini']='CREATE TABLE if not exists {} (renrenId1 varchar(20) NOT NULL,'+' varchar(100),'.join(set(pf_mini.values()))+' varchar(100),lastmodified TIMESTAMP DEFAULT NOW() {})ENGINE=InnoDB DEFAULT CHARSET = utf8;'
+sqls['profile_empty']='CREATE TABLE if not exists {} (renrenId1 varchar(20) NOT NULL,pfStyle varchar(20),lastmodified TIMESTAMP DEFAULT NOW() {})ENGINE=InnoDB DEFAULT CHARSET = utf8;'
+sqls['status']='CREATE TABLE if not exists {} (statusId varchar(20) NOT NULL,renrenId1 varchar(20),timestamp varchar(20),cur_name varchar(50),cur_content varchar(500),orig_owner varchar(20),orig_name varchar(50),orig_content varchar(500),lastmodified TIMESTAMP DEFAULT NOW(),KEY cur_owner(renrenId1),KEY orig_owner(orig_owner) {})ENGINE=InnoDB DEFAULT CHARSET = utf8;'
+key={'name':',KEY idx_temp(renrenId1)','friendList':',KEY idx_temp (renrenId1,renrenId2)','profile_detail':',KEY (renrenId1)','status':',key (statusId)','profile_mini':',KEY (renrenId1)','profile_empty':',KEY (renrenId1)'}
+primary={'name':',PRIMARY KEY (renrenId1)','profile_detail':',PRIMARY KEY (renrenId1)','profile_mini':',PRIMARY KEY (renrenId1)','profile_empty':',PRIMARY KEY (renrenId1)','friendList':',PRIMARY KEY (renrenId1,renrenId2)','status':',PRIMARY KEY (statusId)'}
+
+
+db=repo_mysql()
+print(db)
