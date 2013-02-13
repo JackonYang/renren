ccfebacec548f6362610429dc56f43427641c9d8
JackonYang <jiekunyang@gmail.com>
2013-02-13 15:26:44 +0800

    init get_my_net3.py. mytools is needed to read friendList from pkm

---
+++ renrenSpider/get_my_net3.py
@@ -0,0 +1,20 @@
+import spider
+import mytools
+
+repo_name='orig_renren' #table name prefix
+user='jiekunyang@gmail.com' #renren account
+passwd=None #renren passwd
+
+tt=spider.spider(repo_name,user,passwd)
+my_rid,login_info=tt.login()
+if my_rid is None:
+	print('spider login error.detail:{}'.format(login_info))
+else:
+	print('spider login success.rid={}'.format(my_rid))
+
+	#start to search
+
+	friends=mytools.getFriend()
+	for rid in friends.keys():
+		tt.getNet2(rid)
+		tt.getStatus_friend(rid)
