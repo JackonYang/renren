14c1c2e21dfe153368115e61c0680444cde650de
JackonYang <jiekunyang@gmail.com>
2013-02-13 17:07:11 +0800

    change log level to error

--- renrenSpider/get_my_net3.py
+++ renrenSpider/get_my_net3.py
@@ -6,6 +6,7 @@ user='jiekunyang@gmail.com' #renren account
 passwd=None #renren passwd
 
 tt=spider.spider(repo_name,user,passwd)
+tt.log.setLevel(40)
 my_rid,login_info=tt.login()
 if my_rid is None:
 	print('spider login error.detail:{}'.format(login_info))
@@ -13,8 +14,8 @@ else:
 	print('spider login success.rid={}'.format(my_rid))
 
 	#start to search
-
 	friends=mytools.getFriend()
 	for rid in friends.keys():
+		print('net2 status of {}'.format(rid))
 		tt.getNet2(rid)
 		tt.getStatus_friend(rid)
