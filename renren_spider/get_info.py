06b760b11a95960f272e3594df0f375c75097641
JackonYang <jiekunyang@gmail.com>
2013-03-08 15:51:46 +0800

    unified get_info

---
+++ renren_spider/get_info.py
@@ -0,0 +1,80 @@
+import sys
+import spider
+import mytools
+
+cfg_filename='config_spider.ini'
+
+config=None
+def get_cfg_dict(section_name,has_default=True):
+	global config
+	if config is None:
+		import configparser
+		config=configparser.ConfigParser()
+	# global cfg_filename
+	config.read(cfg_filename)
+	try:
+		cfg=dict(config[section_name].items())
+	except KeyError:
+		# raise error, no such section
+		return None
+	if not has_default:
+		for key in config['DEFAULT'].keys():
+			del(cfg[key])
+	return cfg
+
+def init_config():
+	config_account=get_cfg_dict('account')
+	config_repo=get_cfg_dict('repo')
+
+	repo_name=config_repo['repo_name_pre']   # table name prefix
+	repo_mode=config_repo['mode']   # table name prefix
+	user=config_account['user']  # renren account
+	passwd=config_account['passwd']  # renren passwd
+	# adopt for myself
+	if passwd ==  'None':
+		passwd=None
+	print('config inited')
+	return repo_mode,repo_name,user,passwd
+
+def run(self,meth,orig_id=None):
+	repo_mode,repo_name,user,passwd=init_config()
+	spider.set_repo(repo_mode)
+	tt=spider.spider(repo_name,user,passwd)
+	# tt.log.setLevel(40)
+	my_rid,login_info=tt.login()
+	if my_rid is None:
+		print('spider login error. detail:{}'.format(login_info))
+		return None
+	else:
+		print('spider login success. rid={}'.format(my_rid))
+	if orig_id is None:
+		orig_id = my_rid
+	tt.meth(orig_id)
+
+def pub_meth(obj):
+	meths=set()
+	for meth in dir(obj):
+		if meth.startswith('get'):
+			meths.add(meth)
+	return meths
+
+
+	#get net3
+	#start to search
+	#friends=mytools.getFriend()
+	#for rid in friends.keys():
+	#	tt.getStatus_friend(rid)
+
+if __name__ == '__main__':
+	try:
+		meth=getattr(spider.spider,sys.argv[1])
+	except NameError:
+		print('method {} not definded. method list: {}'.format(sys.argv[1],pub_meth(spider.spider)))
+	else:
+		if len(sys.argv) == 2:
+			orig_id=None
+		elif len(sys.argv) == 3:
+			orig_id=sys.argv[2]
+		else:
+			print('input error. expect: method, renrenId')
+		run(meth,orig_id)
