6fbf2026e512c7f63015ef9b8d69ecadbcf33cf9
JackonYang <jiekunyang@gmail.com>
2013-09-16 21:37:08 +0800

    save and get local-cookie

--- client/browser.py
+++ client/browser.py
@@ -1,103 +1,87 @@
 # -*- Encoding: utf-8 -*-
 import urllib
 from httplib2 import Http
-import pickle
+import os
+
+headers_templates = {
+    'Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.65 Safari/534.24',
+    'Content-type': 'application/x-www-form-urlencoded',
+    'Accept': '*/*',
+    'Accept-Charset': 'UTF-8,*;q=0.5',
+    'Accept-Encoding': 'gzip,deflate,sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    'Cache-Control': 'no-cache',
+}
 
 
 class renren:
-    def __init__(self, autoLogin=True):
+    def __init__(self, user, password, autoLogin=True):
         self.h = Http()
-        self.headers = {
-            'Connection': 'keep-alive',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.65 Safari/534.24',
-            'Content-type': 'application/x-www-form-urlencoded',
-            'Accept': '*/*',
-            'Accept-Charset': 'UTF-8,*;q=0.5',
-            'Accept-Encoding': 'gzip,deflate,sdch',
-            'Accept-Language': 'zh-CN,zh;q=0.8',
-            'Cache-Control': 'no-cache',
-        }
-        self.renrenId = None
-        if autoLogin and (self.renrenId is None):
-            # print 'begin login'
-            self.signin()
+        self.cookie = self.__get_cookie(user, 'renren')
+        if autoLogin and (self.cookie is None):  # check cookie validate
+            self.cookie = self.signin(user, password)
+            # print 'login'
+        else:
+            pass
+            # print 'no need login'
 
-    def signin(self):
-        """sigin to renren.com. return renrenId if success."""
+    def signin(self, user, password):
+        """sigin to renren.com. return cookie if success."""
         # TODO:
         # 1. deal with timeout
         # 2. random useragent
         # 3. more accurate headers
-        # 4. cache cookies in local files
         # 5. deal with verfication code and passwd error
+
         # url
         url = 'http://www.renren.com/PLogin.do'
         home = 'http://www.renren.com/home'
+        # headers
+        headers = headers_templates.copy()
         # body
-        from settings import account
         login_data = {
+            'email': user,
+            'password': password,
             'origURL': home,
             'domain': 'renren.com'
         }
-        login_data.update(account)
         body = urllib.urlencode(login_data)
 
-        rsp, content = self.h.request(url, 'POST', headers=self.headers, body=body)
+        rsp, content = self.h.request(url, 'POST', headers=headers, body=body)
         if '302' == rsp['status']:  # login success
             # print 'login success'
-            self.headers['Cookie'] = rsp['set-cookie']
-            rsp, content = self.h.request(home, "GET", headers=self.headers)
-            import re
-            m = re.compile(r'http://www.renren.com/(\d+)').match(rsp['content-location'])
-            if m is not None:
-                self.renrenId = m.group(1)
+            cookie = rsp['set-cookie']
+            self.__save_cookie(cookie, user, 'renren')
+            return cookie
         else:
-            print rsp['status']
-            with open('home_error.html', 'w') as f:
+            with open('signin_failed_{}.html'.format(user), 'w') as f:
                 f.write(content)
+            return None
 
     def request(self, url, method='GET'):
         return self.h.request(url, method, headers=self.headers)
 
+    def __get_cookie(self, user, website='renren'):
+        filename = '{}_{}.cookie'.format(website, user)
+        cookie = None
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                cookie = f.read().strip(' \n')
+        return cookie
 
-class cookies:
-    def __init__(self, filename='cookies.p'):
-        self.filename = filename
-        self.load()
+    def __save_cookie(self, cookie, user, website='renren'):
+        filename = '{}_{}.cookie'.format(website, user)
+        with open(filename, 'w') as f:
+            f.write(cookie)
 
-    def __del__(self):
-        self.save()
-
-    def load(self):
-        try:
-            with open(self.filename, 'rb') as f:
-                self.cookie = pickle.load(f)
-        except IOError:
-            self.cookie = {}
-
-    def save(self):
-        with open(self.filename, 'wb') as f:
-            pickle.dump(self.cookie, f)
-
-    def add(self, user, website, cookie):
-        self.cookie[(user, website)] = cookie
-
-    def get(self, user, website):
-        ck = self.cookie.get((user, website), None)
-        if ck and self.isValidate(ck):
-            return ck
-        else:
-            return None
-
-    def isValidate(self, cookie):
-        return True
 
 if __name__ == '__main__':
-    rr = renren()
-    print rr.renrenId
-    url = "http://friend.renren.com/GetFriendList.do?curpage={}&id={}".format(0, rr.renrenId)
-    print url
-    rsp, content = rr.request(url)
-    print rsp
-    with open('friendlist.html', 'w') as f:
-        f.write(content)
+    from settings import account
+    renren(account['email'], account['password'])
+    #url = "http://friend.renren.com/GetFriendList.do?curpage={}&id={}".format(0, rr.renrenId)
+    #print url
+    #rsp, content = rr.request(url)
+    #print rsp
+    #with open('friendlist.html', 'w') as f:
+    #    f.write(content)
