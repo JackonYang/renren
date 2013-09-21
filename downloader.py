df3a536cb4c850c0bf97fa5b6cdcdb5ff9d520f2
JackonYang <jiekunyang@gmail.com>
2013-09-22 07:57:22 +0800

    add notes and todo-list

--- downloader.py
+++ downloader.py
@@ -1,4 +1,17 @@
 # -*- Encoding: utf-8 -*-
+"""
+下载并在本地保存人人网数据。
+
+初步解析的数据和 Cookie 均保存在本地 Cache 目录下。
+"""
+# TODO:
+# Cookie
+#   1. request 时，如果 cookie 为空，可以自动加载 cookie 或退出
+#   2. signin 时，在本地保存 cookie
+# local save
+#   3. requestIter 时，本地保存初步解析的数据
+# statistics
+#   4. 保存 requestIter 的性能信息，mean/max/min
 import urllib
 from httplib2 import Http
 import os
