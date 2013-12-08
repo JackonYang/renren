7a4e3b43780e6a878986a6a05f0b797eeb8d87b7
JackonYang <jiekunyang@gmail.com>
2013-12-08 19:11:50 +0800

    fix bugs. error cookies

--- downloader.py
+++ downloader.py
@@ -1,21 +1,11 @@
 # -*- Encoding: utf-8 -*-
 """
 下载并在本地保存人人网数据。
-
-初步解析的数据和 Cookie 均保存在本地 Cache 目录下。
 """
-# TODO:
-# Cookie
-#   1. request 时，如果 cookie 为空，可以自动加载 cookie 或退出
-#   2. signin 时，在本地保存 cookie
-# local save
-#   3. requestIter 时，本地保存初步解析的数据
-# statistics
-#   4. 保存 requestIter 的性能信息，mean/max/min
-import urllib
 from httplib2 import Http
 import os
 import re
+
 import klog.logger as klogger
 from login.renren import login, parseRenrenId
 
@@ -58,7 +48,7 @@ class renren:
 
     def request(self, url, method='GET'):
         """request a page and return html content"""
-        rsp, content = self.h.request(url, method, headers=headers)
+        rsp, content = self.h.request(url, method, headers=self.headers)
         # with open('fl.html', 'w') as f:
         #    f.write(content)
         return content
@@ -107,4 +97,4 @@ if __name__ == '__main__':
     from settings import account
     rr = renren(account['email'], account['password'])
     print rr.renrenId()
-    # print len(rr.friendList(rr.renrenId()))
+    print len(rr.friendList(rr.renrenId()))
