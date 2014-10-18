70794d3b2618bfd66213e2706238125a4279fe2a
Jackon Yang <jiekunyang@gmail.com>
2014-10-18 14:25:47 +0800

    done

--- grabrr.py
+++ grabrr.py
@@ -2,14 +2,15 @@
 """
 下载并在本地保存人人网数据。
 """
+import codecs
 from httplib2 import Http
 import os
 import re
 
-from login.renren import parseRenrenId
-
 maxFailed = 5
-nResend = 3
+resend = 3  # resend times
+itemPtn = re.compile(r'<a href="http://www.renren.com/profile.do\?id=(\d+)">([^<]*)</a>', re.DOTALL)
+
 headers_templates = {
     'Connection': 'keep-alive',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.65 Safari/534.24',
@@ -21,20 +22,22 @@ headers_templates = {
     'Cache-Control': 'no-cache',
 }
 
+
 class renren:
-    def __init__(self, user, password=None):
+    def __init__(self, cookies):
         self.h = Http()
-        # self.headers = headers_templates.copy()
         self.headers = headers_templates.copy()
-        if False: #user.lower() == 'cookie':
-            self.headers['Cookie'] = raw_input('Please input cookie(document.cookie): ')
-        else:
-            #password = raw_input('Please input your password: ')
-            self.headers['Cookie'] = "anonymid=hydr1qdn-ycm6wf; _r01_=1; JSESSIONID=abcKEdr0RzVXX9ifHSzEu; XNESSESSIONID=abcuFh17apWB-7i2oLfFu; depovince=GW; jebecookies=4732ccf8-d2ea-49cc-ad1c-69c8c9cd00be|||||; ick_login=d972250b-250e-4ece-9679-2c064a6763a6; _de=BA3DF646D479464C7CEAE61F3CB0A4C15212E40F3D18115C; p=3dc4a0e170c839b1003c15e0645561b28; ap=539841008; ln_uact=xucengg65243@163.com; ln_hurl=http://hdn.xnimg.cn/photos/hdn321/20140302/2335/h_main_zS6y_227600004407113e.jpg; t=cb5ac4601a607a3f85b1264373f5a76b8; societyguester=cb5ac4601a607a3f85b1264373f5a76b8; id=539841008; xnsid=ec249437; loginfrom=null; feedType=539841008_hot; jebe_key=d608be53-46cd-46e1-956f-9d676d9e907f%7C6fad6b404f16365a47e2b1e47fd7a30d%7C1413377389697%7C1%7C1413377389844; l4pager=1"
+        self.headers['Cookie'] = cookies
+        #"anonymid=hydr1qdn-ycm6wf; _r01_=1; JSESSIONID=abcKEdr0RzVXX9ifHSzEu; XNESSESSIONID=abcuFh17apWB-7i2oLfFu; depovince=GW; jebecookies=4732ccf8-d2ea-49cc-ad1c-69c8c9cd00be|||||; ick_login=d972250b-250e-4ece-9679-2c064a6763a6; _de=BA3DF646D479464C7CEAE61F3CB0A4C15212E40F3D18115C; p=3dc4a0e170c839b1003c15e0645561b28; ap=539841008; ln_uact=xucengg65243@163.com; ln_hurl=http://hdn.xnimg.cn/photos/hdn321/20140302/2335/h_main_zS6y_227600004407113e.jpg; t=cb5ac4601a607a3f85b1264373f5a76b8; societyguester=cb5ac4601a607a3f85b1264373f5a76b8; id=539841008; xnsid=ec249437; loginfrom=null; feedType=539841008_hot; jebe_key=d608be53-46cd-46e1-956f-9d676d9e907f%7C6fad6b404f16365a47e2b1e47fd7a30d%7C1413377389697%7C1%7C1413377389844; l4pager=1"
             # login in
 
     def renrenId(self):
-        return parseRenrenId(self.headers['Cookie'])
+        proj = re.compile(r'\Wid=(\d+);')
+        m = proj.search(self.headers['Cookie'])
+        if m is not None:
+            return m.group(1)
+        else:
+            return None
 
     def request(self, url, method='GET'):
         """request a page and return html content"""
@@ -43,9 +46,7 @@ class renren:
 
     def friendList(self, rid, maxPages=200):
         urlPtn = "http://friend.renren.com/GetFriendList.do?curpage={}&id=" + rid
-        itemPtn = re.compile(r'<a href="http://www.renren.com/profile.do\?id=(\d+)">([^<]*)</a>', re.DOTALL)
         print('\nstart to request {} of {}'.format('friendList', rid))
-        resend = nResend
         pageRange = range(maxPages)
         itemsTotal = set()
         failedSeq = list()
@@ -77,13 +78,24 @@ class renren:
             if itemsMore is None:
                 return None
             itemsTotal.update(itemsMore)
-        filename = 'friendlist_%s.txt' % rid
-        print('\nDone! {} friends got. saved in {}'.format(len(itemsTotal)-1, filename))
-        with open(filename, 'w') as f:
-            f.write(','.join(["@%s(%s)" % one_data for one_data in itemsTotal]))
+        return itemsTotal
+
+def saveFriends(rid, friends, filename, file_encoding='UTF-8'):
+    with codecs.open(filename, 'w', encoding=file_encoding) as f:
+        cont = ' '.join(["@{1}({0})".format(*one_data) for one_data in friends if one_data[0] != rid])
+        f.write(cont.decode(file_encoding))
 
 
 if __name__ == '__main__':
-    email = '0' # raw_input('Please input your email(type `cookie` to login by cookie): ')
-    rr = renren(email)
-    rr.friendList(rr.renrenId())
+    cookies = raw_input('Please input cookies(get from browser by document.cookie): ')
+    rr = renren(cookies)
+    rid = rr.renrenId()
+    friends = rr.friendList(rid)
+
+    file_encoding = 'UTF-8'
+    filename = 'friendlist_%s.txt' % rid
+    saveFriends(rid, friends, filename, file_encoding)
+    print('\nDone! {} friends got. saved in {}'.format(max(len(friends)-1, 0), filename))
+    # self.headers['Cookie'] = raw_input('Please input cookie(document.cookie): ')
+    # if False: #user.lower() == 'cookie':
+            #password = raw_input('Please input your password: ')
