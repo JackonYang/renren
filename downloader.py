fd5d0527b6b80e190bb22d447dd69b6f48bc94ed
Jackon Yang <jiekunyang@gmail.com>
2014-10-18 15:08:52 +0800

    downloader update

--- downloader.py
+++ downloader.py
@@ -6,9 +6,6 @@ from httplib2 import Http
 import os
 import re
 
-import klog.logger as klogger
-from login.renren import login, parseRenrenId
-
 maxFailed = 5
 nResend = 3
 headers_templates = {
@@ -22,28 +19,31 @@ headers_templates = {
     'Cache-Control': 'no-cache',
 }
 
-log = klogger.debugLog()
-
-
 class renren:
-    def __init__(self, user, password):
+    def __init__(self, cookie, log_handler):
         self.h = Http()
         self.headers = headers_templates.copy()
-        self.headers['Cookie'] = login(user, password)
+        self.headers['Cookie'] = cookie
+        self.m_log = log_handler
 
     def renrenId(self):
-        return parseRenrenId(self.headers['Cookie'])
+        proj = re.compile(r'\Wid=(\d+);')
+        m = proj.search(self.headers['Cookie'])
+        if m is not None:
+            return m.group(1)
+        else:
+            return None
 
     def friendList(self, rid, maxPages=100):
         urlPtn = "http://friend.renren.com/GetFriendList.do?curpage={}&id=" + rid
-        itemPtn = re.compile(r'<div class="info">.*?</div>', re.DOTALL)
-        log.info('request {} of {}'.format('friendList', rid))
+        itemPtn = re.compile(r'<a href="http://www.renren.com/profile.do\?id=(\d+)">([^<]*)</a>', re.DOTALL)
+        self.m_log.info('request {} of {}'.format('friendList', rid))
         return self.requestIter(urlPtn, itemPtn, maxPages, nResend)
 
     def status(self, rid, maxPages=100):
         urlPtn = "http://status.renren.com/status?curpage={}&id=" + rid + "&__view=async-html"
         itemPtn = re.compile(r'<li data-wiki\W* id="status-\d+">.*?</li>', re.DOTALL)
-        log.info('request {} of {}'.format('status', rid))
+        self.m_log.info('request {} of {}'.format('status', rid))
         return self.requestIter(urlPtn, itemPtn, maxPages, nResend)
 
     def request(self, url, method='GET'):
@@ -69,23 +69,23 @@ class renren:
                 if itemsPage:
                     itemsTotal.update(itemsPage)
                 else:  # privacy, all pages requested, or safety page
-                    log.debug('nothing contains in page/renrenId {}/{})'.format(page, self.renrenId()))
+                    self.m_log.debug('nothing contains in page/renrenId {}/{})'.format(page, self.renrenId()))
                     break
             else:
-                log.warn('request page/renrenId {}/{} failed'.format(page, self.renrenId()))
+                self.m_log.warn('request page/renrenId {}/{} failed'.format(page, self.renrenId()))
                 failedSeq.append(page)
                 if len(failedSeq) > maxFailed:
-                    log.error('more timeout than {}'.format(maxFailed))
+                    self.m_log.error('more timeout than {}'.format(maxFailed))
                     return None
 
         # deal with timeout_seq
         if failedSeq:
             if resend < 0:
                 return None
-            log.debug('resend failed pages: {}'.format(failedSeq))
-            itemsMore = self.requestIter(urlPtn, itemPtn, failedSeq, resend - 1)
+            self.m_log.debug('resend failed pages: {}'.format(failedSeq))
+            itemsMore = self.requestIter(urlPtn, itemPtn, failedSeq, resend-1)
             if itemsMore is None:
-                return None
+                return None  # return None if not in its integrity
             itemsTotal.update(itemsMore)
         #_safety page check, if itemsAll empty.
         #if (len(itemsAll) ==0) and self._is_safety_page(html_content):
@@ -94,7 +94,17 @@ class renren:
 
 
 if __name__ == '__main__':
-    from settings import account
-    rr = renren(account['email'], account['password'])
+    import logging
+    log = logging.getLogger()
+    logfile = 'debug.log'
+    hdlr = logging.FileHandler(logfile)
+    log.setLevel(logging.DEBUG)
+    hdlr.setFormatter(logging.Formatter('%(asctime)s|%(levelname)s|%(message)s|%(filename)s-%(lineno)s'))
+    log.addHandler(hdlr)
+
+    test_cookie = raw_input('Input cookie(document.cookie): ')
+    print 'logfile: %s' % logfile
+    log.info('Start')
+    rr = renren(test_cookie, log)
     print rr.renrenId()
     print len(rr.friendList(rr.renrenId()))
